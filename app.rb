71e79c4719cc4193b4c7fee8e2f697ad9b76695c
Micah Redding <micahtredding@gmail.com>
2013-12-17 23:53:23 -0600

    Adding blobserver

--- app.rb
+++ app.rb
@@ -2,9 +2,112 @@ require 'sinatra'
 require 'sinatra/form_helpers'
 require 'json'
 
+module Storage
+	Filename = "public/temp.json"
+	def self.write_item key, data
+	  n = self.read_all_items
+	  n[key] = data
+	  self.write_all_items n
+	end
+
+	def self.read_item key
+	  n = self.read_all_items
+	  n[key]
+	end
+
+	def self.delete_item key
+		n = self.read_all_items
+		n.delete(key)
+		self.write_all_items n
+	end
+
+	def self.write_all_items n
+		File.open(Filename,"w") do |f|
+		  f.write(JSON.dump(n))
+		end
+	end
+
+	def self.read_all_items
+		n = {}
+	  File.open(Filename, "r") do |f|
+	    n = JSON.load( f )
+	  end
+	  n
+	end
+end
+
+module Blobserver
+	Filename = "public/blobs.json"
+	def self.put key, data
+	  n = self.read_all_items
+	  n[key] = data
+	  self.write_all_items n
+	end
+
+	def self.get key
+	  n = self.read_all_items
+	  n[key]
+	end
+
+	def self.enumerate
+		self.read_all_items
+	end
+
+	def self.write_all_items n
+		File.open(Filename,"w") do |f|
+		  f.write(JSON.dump(n))
+		end
+	end
+
+	def self.read_all_items
+		blob_hashes = {}
+	  File.open(Filename, "r") do |f|
+	    blob_hashes = JSON.load( f )
+	  end
+	  blob_hashes
+	end
+end
+
+class Blob
+	extend Blobserver
+	attr_accessor :blobref, :content
+	def initialize content
+		@blob_ref = rand(100000).to_s
+		@content = content
+		self.put @blob_ref, @content
+	end
+
+	def self.get key
+		data = super key
+		self.new key, data
+	end
+
+	def self.enumerate
+		blobs = {}
+	  self.read_all_items.each do |blobref, blobcontent|
+	  	blobs[blobref] = Blob.new(blobref, blobcontent)
+	  end
+	  blobs
+	end
+end
+
+class Permanode < Blob
+	claims
+end
+
+class Claim < Blob
+	def self.find_by_permanode
+		blobs = self.enumerate
+		blobs.select { |blob, content|
+			content
+		}
+	end
+end
+
+
+
 class Node
 	attr_accessor :title, :body, :id
-	Filename = "/var/www/sinatra/public/temp.json"
 	@@nodes = {}
 
 	def initialize (node_hash)
@@ -17,18 +120,18 @@ class Node
 		@title = node_hash[:title] || node_hash['title'] || @title
 		@body = node_hash[:body] || node_hash['body'] || @body
 		@@nodes[@id] = self
-		Node.write
+		Node.write_all_items
 	end
 
 	def delete
 		@@nodes.delete(@id)
-		Node.write
+		Node.write_all_items
 	end
 
 	def Node.create(node_hash)
 		node = Node.new(node_hash)
 		@@nodes[node.id] = node
-		Node.write
+		Node.write_all_items
 		node
 	end
 
@@ -40,32 +143,26 @@ class Node
 		@@nodes
 	end
 
-	def Node.read
-	  # {"a":{"title":"title1","body":"body1"},"b":{"title":"title2","body":"body2"},"c":{"title":"title3","body":"body3"}}
-	  File.open( Filename, "r" ) do |f|
-	    n = JSON.load( f )
-	    n.each do |key, node_hash|
-	    	node = Node.new(node_hash)
-	    	@@nodes[key] = node
-	    end
-	  end
+	def Node.read_all_items
+	  n = Storage.read_all_items
+	  n.each do |key, node_hash|
+    	node = Node.new(node_hash)
+    	@@nodes[key] = node
+    end
 	end
 
-	def Node.write
-		n = {}
+	def Node.write_all_items
 		@@nodes.each do |key, node|
-			n[key] = {
+			data = {
 				'id' => key,
 				'title' => node.title,
 				'body' => node.body
 			}
-		end
-		File.open(Filename,"w") do |f|
-		  f.write(JSON.dump(n))
+			Storage.write_item key, data
 		end
 	end
 
-	Node.read
+	Node.read_all_items
 
 end
 
@@ -93,7 +190,6 @@ end
 post '/node/:id/edit' do
 	n = Node.find(params[:id])
 	n.update(params["node"])
-	puts n.inspect
 	redirect '/node'
 end
 
